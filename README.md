6c21461de535b0c6f98bc0f74b2def855f9c568c
Oliver Day <oliver.day.swe@gmail.com>
2024-05-02 20:59:36 -0700

    feat: add basic 'main' function

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # node-web-crawler
+
+To use the web crawler, you need to have Node.js installed on your machine.
+
+To run the web crawler, you need to run the following command:
+
+```bash
+npm run start BASE_URL
+```
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import { argv } from "node:process";
+
+const main = () => {
+  if (argv.length < 3) {
+    console.log("Error: no base_url argument was provided");
+    return;
+  }
+
+  if (argv.length > 3) {
+    console.log("Error: too many arguments were provided");
+    return;
+  }
+
+  const baseURL = argv[2];
+  console.log(`Starting web crawler at baseURL: ${baseURL}...`);
+};
+
+main();
